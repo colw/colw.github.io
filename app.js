209915a4b2ebc354a363a76b2eae52f6a302b978
colw <github.com/colw>
2015-03-01 13:41:45 +0100

    Create individual post pages from markdown files.
    Places posts into their own pages as well as a main index.html file.

--- app.js
+++ app.js
@@ -1,32 +1,64 @@
 var fs = require('fs')
+var path = require('path');
+var mkdirp = require('mkdirp');
 var util = require('util');
 var converter = new (require('showdown')).converter();
-var inputFolder = './posts/';
+var moment = require('moment');
+
+var POST_DIR = 'posts';
+var RAW_POSTS_DIR = './texts/';
+var TEMPLATES = './templates';
 var readOptions = {encoding: 'utf8'};
 
 function isMarkDownFile(fileName) {
-  return fileName.substr(-3) == ".md";
-}
-
-function getDatedContents(fileName) {
-  var f = fs.readFileSync(inputFolder + fileName, readOptions);
-  var d = new Date(f.split('\n')[2]);
-  return {date: d, text: f};
+  return path.extname(fileName) == ".md";
 }
 
 function sortByDateDescending(a, b) {
   return a.date < b.date;
 }
 
-function toHtml(markDownFile) {
-  return '<div class="thing">' + converter.makeHtml(markDownFile.text) + '</div>';
+/* Create a Post Object file */
+function createPostObject(fileName) {
+  var text = fs.readFileSync(path.join(RAW_POSTS_DIR + fileName), readOptions).split('\n');
+  var title = text.splice(0,1)[0];
+  var date = new Date(text.splice(0,3)[1]);
+  
+  var urlName = title.trim().toLowerCase().replace(/\s+/g, '-').replace(/[^a-zA-Z]/g, '');
+
+  var y = '' + date.getFullYear();
+  var m = '' + (date.getMonth()+1);
+  var d = '' + date.getDate();
+  var basepath = path.join(y, m, d);
+  
+  return {title: title, urlname: urlName, basepath: basepath, date: date, text: text.join('\n')};
 }
 
-/* Create an array of processed Markdown files. */
-var markDownFileList = fs.readdirSync(inputFolder).filter(isMarkDownFile);
-var htmlPosts = markDownFileList.map(getDatedContents).sort(sortByDateDescending).map(toHtml);
+/* Transforms a post object into html and writes to a file. */
+function writePostToDirectory(postObject) {
+  mkdirp.sync(path.join(POST_DIR, postObject.basepath));
+  
+  var postTemplate = fs.readFileSync(path.join(TEMPLATES, 'post.template'), readOptions);
+  var fileTemplate = fs.readFileSync(path.join(TEMPLATES, 'index.template'), readOptions);
+  
+  var fmtDate = moment(postObject.date).format('MMMM Do YYYY');
+  var thePost = util.format(postTemplate, postObject.title, fmtDate, converter.makeHtml(postObject.text))
+  
+  var postPage = util.format(fileTemplate, thePost);
+  fs.writeFileSync(path.join(POST_DIR, postObject.basepath, postObject.urlname + '.html'), postPage);
+  
+  return thePost;
+}
 
-/* Concatenate the array and place it into the template html file. */
-var masterFileContents = fs.readFileSync('index.template', readOptions);
-var indexPage = util.format(masterFileContents, htmlPosts.join('\n\n'));
-fs.writeFileSync('index.html', indexPage);
+/* Inserts all posts into a main index.html file */
+function writeIndexPage(htmlPosts) {  
+  var masterFileContents = fs.readFileSync(path.join(TEMPLATES, 'index.template'), readOptions);
+  var indexPage = util.format(masterFileContents, htmlPosts.join('\n\n'));
+  fs.writeFileSync('index.html', indexPage);
+}
+
+var markDownFileList = fs.readdirSync(RAW_POSTS_DIR).filter(isMarkDownFile);
+var postObjects = markDownFileList.map(createPostObject).sort(sortByDateDescending);
+var htmlPosts = postObjects.map(writePostToDirectory);
+
+writeIndexPage(htmlPosts);
